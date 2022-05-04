6107490d0fb612ec7074c48dabbdce3875c9c6f6
Antoine <antoine.koeman@cegeka.com>
2022-05-04 10:58:04 +0200

    Setup project

---
+++ CQRS.UpHealth/CQRS.UpHealth.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CQRS.UpHealth.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
