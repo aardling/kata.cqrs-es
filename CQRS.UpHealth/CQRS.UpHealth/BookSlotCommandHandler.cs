2be30d376f5aee69c980071a63ef6137cf49d68b
Bavo Vander Henst <bavo.vanderhenst@greenstack.be>
2022-05-05 10:39:20 +0200

    Introduced dayschedule domain model

--- CQRS.UpHealth/CQRS.UpHealth/BookSlotCommandHandler.cs
+++ CQRS.UpHealth/CQRS.UpHealth/BookSlotCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using CQRS.UpHealth.Commands;
 using CQRS.UpHealth.CustomExceptions;
+using CQRS.UpHealth.Domain;
 using CQRS.UpHealth.Events;
 
 namespace CQRS.UpHealth;
@@ -17,33 +18,16 @@ public class BookSlotCommandHandler : ICommandHandler<BookSlot>
     {
         var streamId = $"{command.DoctorId}/{command.StartDate.ToString("yyyy/MM/dd")}";
 
-        var foundSlot = false;
         var events = _eventStore.GetEventsByStream(streamId);
-        foreach (var e in events)
-        {
-            if (e is not SlotWasScheduled scheduledEvent)
-            {
-                continue;
-            }
 
-            if(scheduledEvent.SlotId == command.SlotId)
-            {
-                foundSlot = true;
-            }
+        var daySchedule = DaySchedule.FromHistory(events);
+        daySchedule.BookSlot(command.SlotId, command.PatientId);
+
+        var recordedEvents = daySchedule.GetRecordedEvents();
+
+        foreach (var recordedEvent in recordedEvents)
+        {
+            _eventStore.AddEvent(streamId, recordedEvent);
         }
-
-        if(!foundSlot)
-        {
-            throw new UnexistingSlotException();
-        }
-
-        var slotWasBooked = new SlotWasBooked
-        {
-            SlotId = command.SlotId,
-            PatientId = command.PatientId
-        };
-
-        _eventStore.AddEvent(streamId, slotWasBooked);
-
     }
 }
---
+++ CQRS.UpHealth/CQRS.UpHealth/Domain/DaySchedule.cs
@@ -0,0 +1,53 @@
+﻿using CQRS.UpHealth.CustomExceptions;
+using CQRS.UpHealth.Events;
+
+namespace CQRS.UpHealth.Domain;
+
+public class DaySchedule
+{
+    private List<IEvent> _recordedEvents;
+    private List<Guid> _slotIds;
+    
+    private DaySchedule()
+    {
+        _recordedEvents = new List<IEvent>();
+        _slotIds = new List<Guid>();
+    }
+
+    public static DaySchedule FromHistory(IEnumerable<IEvent> historicEvents)
+    {
+        var daySchedule = new DaySchedule();
+        foreach(var historicEvent in historicEvents)
+        {
+            if (historicEvent is not SlotWasScheduled scheduledEvent)
+            {
+                continue;
+            }
+
+            daySchedule._slotIds.Add(scheduledEvent.SlotId);
+        }
+
+        return daySchedule;
+    }
+
+    internal List<IEvent> GetRecordedEvents()
+    {
+        return _recordedEvents;
+    }
+
+    internal void BookSlot(Guid slotId, Guid patientId)
+    {
+        if(!_slotIds.Contains(slotId))
+        {
+            throw new UnexistingSlotException();
+        }
+
+        var slotWasBooked = new SlotWasBooked
+        {
+            SlotId = slotId,
+            PatientId = patientId
+        };
+
+        _recordedEvents.Add(slotWasBooked);
+    }
+}
